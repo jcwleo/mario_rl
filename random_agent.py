a9bba2aeda27af2fb3b919e99cf55bc15b911b37
jcwleo <jcwleo@naver.com>
2018-09-05 00:27:27 +0900

    새로운 parallel env 개발중

--- random_agent.py
+++ random_agent.py
@@ -1,13 +1,42 @@
 from nes_py.wrappers import BinarySpaceToDiscreteSpaceEnv
-from multiprocessing import Process
+from multiprocessing import Process, Pool
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import random
+from multiprocessing import Process
 import time
 
 env = gym_super_mario_bros.make('SuperMarioBros-v2')
 env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
 print(env.action_space.n)
+
+
+class ParallelEnvironment(object):
+    def __init__(self, num_env, num_worker, env_id):
+        self.num_env = num_env
+        self.num_worker = num_worker
+        self.envs = [self._make_env(env_id) for _ in range(num_env)]
+
+    def _make_env(self, env_id):
+        env = gym_super_mario_bros.make(env_id)
+        env = BinarySpaceToDiscreteSpaceEnv(env, SIMPLE_MOVEMENT)
+        return env
+
+    def step(self, actions):
+        obss = []
+        rewards = []
+        dones = []
+        infos = []
+
+        with Pool(processes=self.num_worker) as pool:
+            pool_results = [pool.apply_async(env.step, action) for env, action in zip(self.envs, actions)]
+            results = [res.get() for res in pool_results]
+
+    def _step(self):
+
+    def _reset(self):
+        pass
+
 
 
 def run(env, idx):
