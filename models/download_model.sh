3874cbb372ce8346e6fbdd24e92f3c0d11ac1aa0
chanwoong.joo <chanwoong.joo@navercorp.com>
2018-09-19 12:48:45 +0900

    필요없는 파일 정리

--- models/download_model.sh
+++
@@ -1,6 +0,0 @@
-# 19번 서버에서 MODEL_ID로 구분되는 모델의 (1)config, (2)preprocessor, (3)학습된 parameter를 다운로드
-# 사용법: ./download_model.sh MODEL_ID
-
-#!/bin/bash
-model=$1
-wget 'http://cdev19.nlpr.nhnsystem.com:11230/taewook/dd_model/'$model''
--- models/upload_model.sh
+++
@@ -1,6 +0,0 @@
-# MODEL_ID로 구분되는 모델의 (1)config, (2)preprocessor, (3)학습된 parameter를 19번 서버에 업로드
-# 사용법: ./upload_model.sh MODEL_ID
-
-#!/bin/bash
-model=$1
-rsync -avr $model cdev19.nlpr::R/home1/irteam/apache/taewook/dd_model
